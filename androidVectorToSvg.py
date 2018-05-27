a4284317d795f7a8d814f53b1bea414f7c77ccda
JRock007 <github@jean-romain.com>
2018-05-27 11:06:33 +0200

    Add compatibility for more features from Android vector format
    - clipPaths are now created and applied to groups
    - groups can now be translated, rotated and scaled
    - alpha tags are supported

--- androidVectorToSvg.py
+++ androidVectorToSvg.py
@@ -3,12 +3,21 @@ import argparse
 from lxml import etree
 
 
+"""
+TODO: - Add gradient color compatibility
+      - Keep order of Android vector (right now, paths that were drawn on top are drawn at the bottom)
+      - Apply clipPaths even if not in a group
+"""
+
+
 def main(inputfile, outputfile):
-    global namespace
+    # https://developer.android.com/reference/android/graphics/drawable/VectorDrawable
+    global namespace, clipPaths
     try:
         tree = etree.parse(inputfile)
         root = tree.getroot()
         namespace = root.nsmap["android"]
+        clipPaths = []  # Clip paths are added in <defs> at the end
 
         newFile = createRoot(root)
         for child in root:
@@ -16,8 +25,19 @@ def main(inputfile, outputfile):
                 createPath(newFile, child)
             elif child.tag == "group":
                 createGroup(newFile, child)
+            elif child.tag == "clip-path":
+                createClipPath(newFile, child)
             else:
                 pass
+
+        # Define the clipPaths, if there are any
+        if len(clipPaths) > 0:
+            defs = etree.SubElement(newFile, "defs")
+            for i in range(len(clipPaths)):
+                data = clipPaths[i]
+                pathID = "clipPath{}".format(i)
+                clip = etree.SubElement(defs, "clipPath", id=pathID)
+                etree.SubElement(clip, "path", d=data)
 
         et = etree.ElementTree(newFile)
         et.write(outputfile, pretty_print=True, xml_declaration=False, encoding="utf-8")
@@ -38,12 +58,18 @@ def createRoot(vector):
     height = getAttribute(vector, "height").replace("dp", "")
     viewportWidth = getAttribute(vector, "viewportWidth").replace("dp", "")
     viewportHeight = getAttribute(vector, "viewportHeight").replace("dp", "")
+
+    if hasAttribute(vector, "alpha"):
+        alpha = getAttribute(vector, "fillColor")
+        vector.set("opacity", alpha)
+
     root = etree.Element("svg", xmlns="http://www.w3.org/2000/svg", width=width, height=height, viewBox="0 0 {} {}".format(viewportWidth, viewportHeight))
     return root
 
 
 def createPath(root, path):
     data = getAttribute(path, "pathData")
+    data = " ".join(data.split())  # Remove duplicate white-spaces
     child = etree.SubElement(root, "path", d=data)
 
     if hasAttribute(path, "fillColor"):
@@ -51,6 +77,14 @@ def createPath(root, path):
         child.set("fill", color)
     else:
         child.set("fill", "none")
+
+    if hasAttribute(path, "fillType"):
+        rule = getAttribute(path, "fillType")
+        child.set("fill-rule", rule)
+
+    if hasAttribute(path, "fillAlpha"):
+        alpha = getAttribute(path, "fillAlpha")
+        child.set("fill-opacity", alpha)
 
     if hasAttribute(path, "strokeColor"):
         color = getAttribute(path, "strokeColor")
@@ -60,13 +94,17 @@ def createPath(root, path):
         width = getAttribute(path, "strokeWidth")
         child.set("stroke-width", width)
 
-    if hasAttribute(path, "strokeLineJoin"):
-        join = getAttribute(path, "strokeLineJoin")
-        child.set("stroke-linejoin", join)
+    if hasAttribute(path, "strokeAlpha"):
+        alpha = getAttribute(path, "strokeAlpha")
+        child.set("stroke-opacity", alpha)
 
     if hasAttribute(path, "strokeLineCap"):
         cap = getAttribute(path, "strokeLineCap")
         child.set("stroke-linecap", cap)
+
+    if hasAttribute(path, "strokeLineJoin"):
+        join = getAttribute(path, "strokeLineJoin")
+        child.set("stroke-linejoin", join)
 
     if hasAttribute(path, "strokeMiterLimit"):
         lim = getAttribute(path, "strokeMiterLimit")
@@ -76,16 +114,59 @@ def createPath(root, path):
 def createGroup(root, group):
     g = etree.SubElement(root, "g")
 
-    if hasAttribute(group, "translateX") and hasAttribute(group, "translateY"):
-        x = getAttribute(group, "translateX")
-        y = getAttribute(group, "translateY")
-        g.set("transform", "translate({}, {})".format(x, y))
+    # Initialize values to specs' defaults so setting them won't change anything visually by default
+    translateX, translateY, rotate, pivotX, pivotY, scaleX, scaleY = 0, 0, 0, 0, 0, 1, 1
 
-    for child in root:
+    # Change values according to xml
+    if hasAttribute(group, "translateX") and hasAttribute(group, "translateY"):
+        translateX = getAttribute(group, "translateX")
+        translateY = getAttribute(group, "translateY")
+
+    if hasAttribute(group, "rotation"):
+        rotate = getAttribute(group, "rotation")
+
+    if hasAttribute(group, "pivotX") and hasAttribute(group, "pivotY"):
+        pivotX = getAttribute(group, "pivotX")
+        pivotY = getAttribute(group, "pivotY")
+
+    if hasAttribute(group, "scaleX") and hasAttribute(group, "scaleY"):
+        scaleX = getAttribute(group, "scaleX")
+        scaleY = getAttribute(group, "scaleY")
+    elif hasAttribute(group, "scaleX"):
+        scaleX = getAttribute(group, "scaleX")
+        scaleY = scaleX
+    elif hasAttribute(group, "scaleY"):
+        scaleY = getAttribute(group, "scaleY")
+        scaleX = scaleY
+
+    # Apply to group
+    g.set("transform", "translate({} {}) rotate({} {} {}) scale({} {})".format(translateX, translateY, rotate, pivotX, pivotY, scaleX, scaleY))
+
+    # Create subelements of group
+    for child in group:
         if child.tag == "path":
             createPath(g, child)
+        elif child.tag == "group":
+            createGroup(g, child)
+        elif child.tag == "clip-path":
+            createClipPath(g, child)
         else:
             pass
+
+
+def createClipPath(root, clip):
+    data = getAttribute(clip, "pathData")
+    data = " ".join(data.split())  # Remove duplicate white-spaces
+
+    # Save clipPath
+    pathID = len(clipPaths)
+    clipPaths.append(data)
+
+    # Apply clipPath
+    if root.tag == "g":
+        root.set("clip-path", "url(#clipPath{})".format(pathID))
+    else:
+        print("clip path in main svg")
 
 
 def displayHelp():
